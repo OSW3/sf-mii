aed990a57a2b54493169b19ab3318eb1089d67b3
Arnaud BODEL <contact@osw3.net>
2022-06-01 11:38:36 +0200

    -

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BookType extends AbstractType
@@ -49,6 +50,10 @@ class BookType extends AbstractType
                     new NotBlank([
                         'message' => "Le titre du livre est obligatoire",
                     ]),
+                    new Length([
+                        'max' => 180,
+                        'maxMessage' => "Le titre ne peut contenir que {{ limit }} caractères"
+                    ])
                 ],
             ])
 
@@ -80,6 +85,8 @@ class BookType extends AbstractType
             ])
 
             // Cover (FileType)
+            // Type image .jpg, .jpeg, .png
+            // Taille 2Mo
             ->add('cover', FileType::class, [
 
                 // Label
@@ -105,6 +112,7 @@ class BookType extends AbstractType
             ])
 
             // Price (NumberType)
+            // Supérieur à zero
             ->add('price', NumberType::class, [
 
                 // Label
